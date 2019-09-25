2b2e2a522ac9e048c51ceba1a15a1c1360abb60a
Carlos <cvcaban@feralcat.xyz>
2019-09-25 08:17:25 -0700

    React table

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactTable from "react-table";
+import 'react-table/react-table.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const columns = [{
+  Header: 'Name',
+  accessor: 'name' // String-based value accessors!
+}, {
+  Header: 'Age',
+  accessor: 'age',
+  Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
+}, {
+  id: 'friendName', // Required because our accessor is not a string
+  Header: 'Friend Name',
+  accessor: d => d.friend.name // Custom value accessors!
+}, {
+  Header: props => <span>Friend Age</span>, // Custom header components!
+  accessor: 'friend.age'
+}]
+class App extends React.Component {
+    constructor(props){
+    super(props)
+
+  }
+
+  fetchingData(){
+      console.log('Firing fetch data')
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <button onClick={() => {
+            this.refReactTable.fireFetchData()
+          }}>Refresh
+          </button>
+          <ReactTable
+              columns={columns}
+              manual
+              ref={(refReactTable) => {
+                this.refReactTable = refReactTable;
+              }}
+              onFetchData={this.fetchingData}
+          />
+        </div>
+    );
+  }
 }
+
+
+
 
 export default App;
